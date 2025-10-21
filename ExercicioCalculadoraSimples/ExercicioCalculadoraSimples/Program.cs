17bcdd6778dc009648ecab64efa2c4689932eb40
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:52:59 -0300

    add: Exercício de uma calculadora simples

---
+++ ExercicioCalculadoraSimples/ExercicioCalculadoraSimples/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExercicioCalculadoraSimples;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("======== Bem-vindo a nossa calculadora ========");
+        Console.WriteLine("Digite um número: ");
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite outro número: ");
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Agora digite a operação (+, -, *, /): ");
+
+        string operation = Console.ReadLine();
+
+        if (operation == "+")
+        {
+            Console.WriteLine(number1 + number2);
+        }
+        else if (operation == "-")
+        {
+            Console.WriteLine(number1 - number2);
+        }
+        else if (operation == "*")
+        {
+            Console.WriteLine(number1 * number2);
+        }
+        else if (operation == "/")
+        {
+            Console.WriteLine(number1 / number2);
+        }
+        else
+        {
+            Console.WriteLine("Digite um operador válido.");
+        }
+
+    }
+}
