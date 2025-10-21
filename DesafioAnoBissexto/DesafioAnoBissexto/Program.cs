79edebe50c6559fb126c8cba92369255fa025d7e
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:46:08 -0300

    add: Desafio do ano bissexto

---
+++ DesafioAnoBissexto/DesafioAnoBissexto/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DesafioAnoBissexto;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("========== Bem-vindo ao verificador de ano bissexto ==========");
+        Console.WriteLine("Digite o número do ano que deseja verificar: ");
+
+        int year = Convert.ToInt32(Console.ReadLine());
+
+
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine("Ano bissexto!");
+        } else
+        {
+            Console.WriteLine("Esse ano NÃO é bissexto.");
+        }
+    }
+}
