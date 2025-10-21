cabb0005d48f17f64cf4abfc57d000fb7ec6efa4
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:57:45 -0300

    add: Exercício de maioridade

---
+++ ExercicioMaioridade/ExercicioMaioridade/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExercicioMaioridade
+{
+    public class Program {
+
+        public static void Main ()
+        {
+            Console.WriteLine("======= Bem-vindo ao verificador de maioridade =======");
+            Console.WriteLine("Digite a sua idade: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if ( age >= 18 )
+            {
+                Console.WriteLine("Que ótimo! Você é maior de idade");
+            } else
+            {
+                Console.WriteLine("Infelizmente você não possui maioridade.");
+            }
+        }
+
+    }
+}
+
