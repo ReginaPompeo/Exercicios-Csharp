ef7bc5335bcce50bd21458d74d1227e6ea23e1d4
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:59:11 -0300

    add: Exercício de número positivo/negativo

---
+++ ExercicioNumeroPositivoNegativo/ExercicioNumeroPositivoNegativo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace ExercicioNumeroPositivoNegativo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("========== Verificador de número ==========");
+            Console.WriteLine("Digite um número: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("O número " + number + " é positivo.");
+
+            } else if (number < 0) 
+            {
+                Console.WriteLine("O número " + number + " é negativo."); 
+
+            } else
+            {
+                Console.WriteLine("O número é zero.");
+            }
+        }
+    }
+}
