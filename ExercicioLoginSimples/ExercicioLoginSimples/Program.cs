734da5bef5bc8cf7ba142ff69faf1d982c1d6681
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:54:43 -0300

    add: Exercício de login simples

---
+++ ExercicioLoginSimples/ExercicioLoginSimples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata;
+
+namespace ExercicioLoginSimples
+{
+    public class Program
+    {
+        public static void Main ()
+        {
+            string user = "ReginaP";
+            string password = "Regina123";
+
+            Console.WriteLine("========== Bem-vindo ao nosso aplicativo ==========");
+            Console.WriteLine("Primeiro faça seu login");
+            Console.WriteLine("Insira seu nome de usuário: ");
+
+            string userAnswer = Console.ReadLine();
+
+            Console.WriteLine("Agora insira a sua senha: ");
+
+            string passwordAnswer = Console.ReadLine();
+
+            if (userAnswer == user && passwordAnswer == password)
+            {
+                Console.WriteLine("Seu login foi realizado!");
+            } else if (userAnswer == user && passwordAnswer != password)
+            {
+                Console.WriteLine("Essa senha é inválida");      
+            } else
+            {
+                Console.WriteLine("Usuário e senha inválidos.");
+            }
+
+
+
+        }
+    }
+}
