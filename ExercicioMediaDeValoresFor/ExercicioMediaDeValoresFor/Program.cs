558a5b96405b86fe1ef5006195f5544643f4d9b2
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:56:18 -0300

    add: Exercício de média de valores usando for

---
+++ ExercicioMediaDeValoresFor/ExercicioMediaDeValoresFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExercicioMediaDeValoresFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Média de valores =====");
+
+        int soma = 0;
+        int quantidade = 5;
+
+        for (int i = 1; i <= quantidade; i++)
+        {
+            Console.WriteLine($"Digite o {i}º número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            soma += numero;
+        }
+
+        double media = (double)soma / quantidade;
+        Console.WriteLine($"A média dos {quantidade} números é: {media}");
+    }
+}
