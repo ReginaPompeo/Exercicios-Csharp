22ba085e48f9834544a1b90726a43b308a582c97
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:03:18 -0300

    add: Exercício de senha com while

---
+++ ExercicioSenhaWhile/ExercicioSenhaWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace ExercicioSenhaWhile;
+
+public class Program
+{
+    public static void Main()
+    {
+        string user = "Regina";
+        string password = "1234";
+
+        Console.WriteLine("===== Login e senha =====");
+
+        Console.WriteLine("Digite seu nome de usuário: ");
+        string userAnswer = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua senha: ");
+        string passwordAnswer = Console.ReadLine();
+
+
+        while (userAnswer != user || passwordAnswer != password)
+        {
+            Console.WriteLine("Usuário ou senha incorretos. Tente novamente!");
+
+            Console.WriteLine("Digite seu nome de usuário: ");
+            userAnswer = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua senha: ");
+            passwordAnswer = Console.ReadLine();
+
+        }
+        Console.WriteLine("Você está logado!");
+    }
+}
