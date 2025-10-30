144030f35d563cc0709e2795b9c7995235e7d6ee
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:59:52 -0300

    add: Exercício das notas escolares usando switch

---
+++ ExercicioNotasEscolaresSwitch/ExercicioNotasEscolaresSwitch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace ExercicioNotasEscolaresSwitch;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Notas Escolares =====");
+        Console.WriteLine("Digite a sua nota (A, B, C, D, F): ");
+
+        String note = Console.ReadLine();
+
+        switch (note)
+        {
+            case "A":
+                Console.WriteLine("Excelente!");
+                break;
+
+            case "B":
+                Console.WriteLine("Bom");
+                break;
+
+            case "C":
+                Console.WriteLine("Regular");
+                break;
+
+            case "D":
+                Console.WriteLine("Ruim");
+                break;
+
+            case "F":
+                Console.WriteLine("Reprovado");
+                break;
+
+            default:
+                Console.WriteLine("Nota inválida.");
+                break;
+        }
+    }
+}
+
