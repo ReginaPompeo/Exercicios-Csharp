d713d708cb81a55fa3ca6705d503b42e3c91539a
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:05:34 -0300

    add: Exercício de tabuada usando for

---
+++ ExercicioTabuadaFor/ExercicioTabuadaFor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExercicioTabuadaFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Tabuada com For =====");
+        Console.WriteLine("Digite um número de tabuada de 1 a 10: ");
+        int tabuada = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Tabuada do {tabuada}: ");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i * tabuada);
+        }
+
+        Console.WriteLine("Obrigada por utilizar a nossa tabuada online!");
+    }
+}
