a457f192f081a8fc57f008721efe1682fa3cd25f
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:43:40 -0300

    add: Contagem regressiva usando o for

---
+++ ContagemRegressivaFor/ContagemRegressivaFor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ContagemRegressivaFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Contagem regressiva com for =====");
+
+        for (int i = 10; i <= 10 && i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Fim da contagem regressiva!l7ç");
+    }
+}
