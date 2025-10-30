998013539ce373fbd8dede7c260e1c7912c7bcaf
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:04:10 -0300

    add: Exercício de soma usando o for

---
+++ ExercicioSomaDeNumerosFor/ExercicioSomaDeNumerosFor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioSomaDeNumerosFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Soma de números de 1 a 100 =====");
+
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            soma += i;
+
+        }
+        Console.WriteLine($"A soma é de {soma}");
+    }
+}
