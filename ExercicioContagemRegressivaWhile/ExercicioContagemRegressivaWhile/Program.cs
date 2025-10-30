fc48300ada9eca4a40035d29d3308f72cd72051f
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:52:08 -0300

    add: Exercício de contagem regressiva usando while

---
+++ ExercicioContagemRegressivaWhile/ExercicioContagemRegressivaWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioContagemRegressivaWhile;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Contagem Regressiva =====");
+        Console.WriteLine("Digite um número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        while (number != 0) 
+        {
+            Console.WriteLine(number);
+            --number;
+        }
+        Console.WriteLine("0 \nFim!");
+    } 
+}
