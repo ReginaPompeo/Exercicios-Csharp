bf59b4261bf93601a9968e230a078f10ecb62c50
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:04:56 -0300

    add: Exercício de soma usando o while

---
+++ ExercicioSomaDeNumerosWhile/ExercicioSomaDeNumerosWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioSomaDeNumerosWhile;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Soma de número com While");
+        Console.WriteLine("Digite um número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sumOthers = number;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Digite um número: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            sumOthers = number + sumOthers;
+        }
+
+        Console.WriteLine("Parabéns! Você chegou ao número 0, a soma das tentativas foi: " + sumOthers);
+    }
+}
