5ba5ca10fba718508af0cadcab2065e8b23ec72c
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:54:59 -0300

    add: Exercício fatorial usando for

---
+++ ExercicioFatorialFor/ExercicioFatorialFor/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercicioFatorialFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Digite um número fatorial =====");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int fatorial = 1;
+
+        for (int i = number1; i >= 1; i--)
+        {
+            fatorial *= i;
+        }
+        Console.WriteLine($"O fatorial de {number1} é: {fatorial}");
+    }
+}
