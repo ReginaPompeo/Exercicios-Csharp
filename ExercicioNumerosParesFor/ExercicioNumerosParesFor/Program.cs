c38470e8cb9aeac3a63bd094477fb2692d5f22eb
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:00:51 -0300

    add: Exercício de números pares usando for

---
+++ ExercicioNumerosParesFor/ExercicioNumerosParesFor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercicioNumerosParesFor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Números pares de 1 a 20 =====");
+
+        for (int i = 2; i <=20; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
