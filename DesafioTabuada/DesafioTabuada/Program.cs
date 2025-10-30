5343614c5401b31170eadef0fc7aed4b586be992
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:50:03 -0300

    add: Desafio da tabuada

---
+++ DesafioTabuada/DesafioTabuada/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DesafioTabuada;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Seja bem-vindo a tabuada interativa! =====");
+       
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.WriteLine("Digite um número para a tabuada do 1 ao 10: ");
+            int tabuada = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tabuada do " + tabuada + ": ");
+
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(tabuada * i);
+                i++;
+            }
+            Console.WriteLine("Deseja ver outra tabuada? (S/N): ");
+            string resposta = Console.ReadLine();
+
+            if ( resposta == "N")
+            {
+                continuar = false;
+            }
+        }
+        Console.WriteLine("Obrigada por usar a tabuada interativa!");
+    }
+}
