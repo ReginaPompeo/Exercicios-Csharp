6472c69f7a89efafc356e341c1f2342e9b9df32c
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 14:06:18 -0300

    add: Exercício de 1 a 10 com while

---
+++ ExercicioWhile1A10/ExercicioWhile1A10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercicioWhile1A10;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== While de 1 a 10 =====");
+
+        int i = 0;
+
+        while (i <= 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
